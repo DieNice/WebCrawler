c0b0010ea71d9f8df29d4ce72b4362f104d4c22f
DieNice <DailyPlanning@bk.ru>
2021-06-15 20:53:04 +1000

    ref

--- SentimentAnalyzer/analyzer.py
+++ SentimentAnalyzer/analyzer.py
@@ -1,37 +1,40 @@
 import logging
+from typing import Tuple
+from operator import itemgetter
+import click
+import fasttext
 from dostoevsky.tokenization import RegexTokenizer
 from dostoevsky.models import FastTextSocialNetworkModel
-from Mongodb.config import DevelopingConfig
-from Mongodb.models import Page
-import string
-import nltk
-import click
 from nltk import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-import fasttext
-from typing import Tuple
-from operator import itemgetter
+from Mongodb.config import DevelopingConfig
+from Mongodb.models import Page
 
 
 class SentimentAnalyzer():
-    def __init__(self):
+    '''Class for sentiment analyzis'''
+
+    def __init__(self) -> None:
         self.tokenizer = RegexTokenizer()
         logging.info("set tokenizer")
         fasttext.FastText.eprint = lambda x: None
         self.stop_words = stopwords.words('russian')
         logging.info("set sropwords")
+        self.connect = None
 
-    def connect_to_db(self, namedb, usr, pwd, port):
+    def connect_to_db(self, namedb, usr, pwd, port) -> None:
+        '''Connection to db'''
         self.connect = DevelopingConfig(namedb, usr, pwd, port)
         logging.info("Sentiment analyzer connected to database")
 
     def __get_data(self):
-        return Page.objects()
+        return Page.objects
 
     def __prepare(self, text):
         sentences = sent_tokenize(text)
-        for i in range(len(sentences)):
+        l_sent = len(sentences)
+        for i in range(l_sent):
             sentences[i] = sentences[i].replace('\n\n', '')
             tokens = word_tokenize(sentences[i])
             words = [word for word in tokens if word.isalpha()]
@@ -63,9 +66,15 @@ class SentimentAnalyzer():
         result_list.append((Page.objects.average('skip_sentiment'), 'Skip sentiment'))
         result_list.sort(key=itemgetter(0))
         print(
-            f"Justification:{result_list[0][1]}({result_list[0][0]})<{result_list[1][1]}({result_list[1][0]})<{result_list[2][1]}({result_list[2][0]})<{result_list[3][1]}({result_list[3][0]})")
+            f"Justification:{result_list[0][1]}({result_list[0][0]})<"
+            f"{result_list[1][1]}({result_list[1][0]})<"
+            f"{result_list[2][1]}({result_list[2][0]})<"
+            f"{result_list[3][1]}({result_list[3][0]})")
         logging.info(
-            f"Justification:{result_list[0][1]}({result_list[0][0]})<{result_list[1][1]}({result_list[1][0]})<{result_list[2][1]}({result_list[2][0]})<{result_list[3][1]}({result_list[3][0]})")
+            f"Justification:{result_list[0][1]}({result_list[0][0]})<"
+            f"{result_list[1][1]}({result_list[1][0]})<"
+            f"{result_list[2][1]}({result_list[2][0]})<"
+            f"{result_list[3][1]}({result_list[3][0]})")
 
     def __count_result_value(self) -> Tuple[float, str]:
         ''':return max sentiment value and description'''
@@ -82,62 +91,69 @@ class SentimentAnalyzer():
                 description = desc
         return maximum, description
 
-    def analyze(self):
+    def __culculate_sentiment(self, sentiment_values) -> Tuple[float, float, float, float]:
+        count_positive = 0
+        sum_positive = 0
+        count_negative = 0
+        sum_negative = 0
+        count_neutral = 0
+        sum_neutral = 0
+        count_skip = 0
+        sum_skip = 0
+        for result in sentiment_values:
+            if 'neutral' in result:
+                count_neutral += 1
+                sum_neutral += result['neutral']
+            if 'positive' in result:
+                count_positive += 1
+                sum_positive += result['positive']
+            if 'negative' in result:
+                count_negative += 1
+                sum_negative += result['negative']
+            if 'skip' in result:
+                count_skip += 1
+                sum_skip += result['skip']
+        try:
+            result_positive = sum_positive / count_positive
+        except ZeroDivisionError:
+            result_positive = 0
+        logging.info(f"result positive value are counted {result_positive}")
+        try:
+            result_negative = sum_negative / count_negative
+        except ZeroDivisionError:
+            result_negative = 0
+        logging.info(f"result negative value are counted {result_negative}")
+        try:
+            result_neutral = sum_neutral / count_neutral
+        except ZeroDivisionError:
+            result_neutral = 0
+        logging.info(f"result neutral value are counted {result_neutral}")
+        try:
+            result_skip = sum_skip / count_skip
+        except ZeroDivisionError:
+            result_skip = 0
+        logging.info(f"result skip value are counted {result_skip}")
+        return result_positive, result_negative, result_neutral, result_skip
+
+    def __save_page(self, page: Page, values: Tuple[float, float, float, float]) -> None:
+        page.positive_sentiment = values[0]
+        page.negative_sentiment = values[1]
+        page.neutral_sentiment = values[2]
+        page.skip_sentiment = values[3]
+        page.save()
+
+    def analyze(self) -> None:
+        '''All pages analyze'''
         pages = self.__get_data()
-        with click.progressbar(pages, label="Sentiment analyse") as bar:
-            for page in bar:
+        with click.progressbar(pages, label="Sentiment analyse") as all_pages:
+            for page in all_pages:
                 model = FastTextSocialNetworkModel(tokenizer=self.tokenizer, lemmatize=True)
                 sentences = self.__prepare(page.content)
                 logging.info(f"page {page.title} prepared")
                 results = model.predict(sentences, k=2)
                 logging.info(f"page {page.title} predicted")
-                count_positive = 0
-                sum_positive = 0
-                count_negative = 0
-                sum_negative = 0
-                count_neutral = 0
-                sum_neutral = 0
-                count_skip = 0
-                sum_skip = 0
-                for result in results:
-                    if 'neutral' in result:
-                        count_neutral += 1
-                        sum_neutral += result['neutral']
-                    if 'positive' in result:
-                        count_positive += 1
-                        sum_positive += result['positive']
-                    if 'negative' in result:
-                        count_negative += 1
-                        sum_negative += result['negative']
-                    if 'skip' in result:
-                        count_skip += 1
-                        sum_skip += result['skip']
-                try:
-                    result_positive = sum_positive / count_positive
-                except ZeroDivisionError:
-                    result_positive = 0
-                logging.info(f"result positive value are counted {result_positive}")
-                try:
-                    result_negative = sum_negative / count_negative
-                except ZeroDivisionError:
-                    result_negative = 0
-                logging.info(f"result negative value are counted {result_negative}")
-                try:
-                    result_neutral = sum_neutral / count_neutral
-                except ZeroDivisionError:
-                    result_neutral = 0
-                logging.info(f"result neutral value are counted {result_neutral}")
-                try:
-                    result_skip = sum_skip / count_skip
-                except ZeroDivisionError:
-                    result_skip = 0
-                logging.info(f"result skip value are counted {result_skip}")
-
-                page.positive_sentiment = result_positive
-                page.negative_sentiment = result_negative
-                page.neutral_sentiment = result_neutral
-                page.skip_sentiment = result_skip
-                page.save()
+                values = self.__culculate_sentiment(results)
+                self.__save_page(page, values)
                 logging.info(f"results of sentiment values of {page.title} are saved")
         result_value, description = self.__count_result_value()
         print(f"{description}:{result_value}")
